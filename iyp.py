8b662ba47a6a2f6926a593634ffddac787ade562
soumyaranjanbhanja <soumyaranjanb@gmail.com>
2023-06-22 14:56:00 +0530

    hello guys its my fourth python program

---
+++ iyp.py
@@ -0,0 +1,16 @@
+#!/bin/usr/python3
+class Test:
+    __sam="hello guys"
+    sam="ui"
+    typ="soumyaTwo"
+    """created class Test"""
+    def __init__(self):
+               print("Non parameterised constructor")
+    def Add(self,a,b):
+               print("Addition :",a+b)
+    def Sub(self,a,b):
+               print("Subtraction :",a-b)
+               print(self.__sam)
+y=Test()
+y.Sub(8,9)
+print(d._Test__sam)              
